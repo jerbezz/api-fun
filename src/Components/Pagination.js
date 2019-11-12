5e6d2c748fe185dcb36b6061c500cb090a5832af
Jeremy Bezzant <jerbezz@gmail.com>
2019-11-12 14:21:46 -0700

    next page fx

--- src/Components/Pagination.js
+++ src/Components/Pagination.js
@@ -16,6 +16,21 @@ const Pagination = (url, planets, resolve, reject) => {
       reject('Something wrong. Please refresh the page and try again.')
     })
 
+    const nextPage = (url, planets, resolve, reject) => {
+      axios.get(url)
+        .then(response => {
+          const retrivedPlanets = planets.concat(response.data.results)
+          if (response.data.next !== null) {
+            getPlanets(response.data.next, retrivedPlanets, resolve, reject)
+          } else {
+            resolve(retrivedPlanets)
+          }
+        })
+        .catch(error => {
+          console.log(error)
+          reject('Something wrong. Please refresh the page and try again.')
+        })
+
     return (
         <div>
             
